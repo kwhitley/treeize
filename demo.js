d5a22dc77b7a38697ec68fd3b356b01a42cee874
Kevin Whitley <kevin3503@gmail.com>
2014-09-19 11:59:45 +0300

    breaking out intro to node traversal, resultasobject separated

--- demo.js
+++ demo.js
@@ -19,6 +19,7 @@ fields
 
 var testNodeDependency = [
   {
+    'id':1,
     'user:name': 'kevin',
     'user:age': 34
   }
@@ -33,11 +34,11 @@ test4
 // var keywords = new Treeize();
 // keywords.grow(keywordsTest);
 
+console.log('BASE>', test4 + '');
+console.log('STATS>', util.inspect(test4.stats, false, null), "\n\n");
+
 // console.log('BASE>', fields + '');
 // console.log('STATS>', util.inspect(fields.stats, false, null), "\n\n");
-
-console.log('BASE>', fields + '');
-console.log('STATS>', util.inspect(fields.stats, false, null), "\n\n");
 
 // console.log('KEYWORDS>', keywords + '');
 // console.log('STATS>', util.inspect(keywords.stats, false, null));
