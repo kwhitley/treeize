d7544d6bef99474a4e5347aec0541a047dc5c710
Kevin Whitley <kevin3503@gmail.com>
2014-09-19 13:32:10 +0300

    added deep object node creation

--- demo.js
+++ demo.js
@@ -25,25 +25,35 @@ var fields = new Treeize();
 var testNodeDependency = [
   {
     'id':1,
-    'user:name': 'kevin',
+    'user:a:b:c:d:e': 'kevin',
+    'user:age': 34
+  },
+  {
+    'id':1,
+    'user:a:b:c:def:e': 'jimbo',
+    'user:age': 34
+  },
+  {
+    'id':1,
+    'user:a:b:c:d:efg': 'kelly',
     'user:age': 34
   }
 ];
 
-// var test4 = new Treeize();
-// test4
-//   .setOptions({ log: true })
-//   .grow(testNodeDependency)
-// ;
+var test4 = new Treeize();
+test4
+  .setOptions({ log: true, input: { uniformRows: false } })
+  .grow(testNodeDependency)
+;
 
 // var keywords = new Treeize();
 // keywords.grow(keywordsTest);
 
-// console.log('BASE>', test4 + '');
-// console.log('STATS>', util.inspect(test4.stats, false, null), "\n\n");
+console.log('BASE>', test4 + '');
+console.log('STATS>', util.inspect(test4.stats, false, null), "\n\n");
 
-console.log('BASE>', fields + '');
-console.log('STATS>', util.inspect(fields.stats, false, null), "\n\n");
+// console.log('BASE>', fields + '');
+// console.log('STATS>', util.inspect(fields.stats, false, null), "\n\n");
 
 // console.log('KEYWORDS>', keywords + '');
 // console.log('STATS>', util.inspect(keywords.stats, false, null));
