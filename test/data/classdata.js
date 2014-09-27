38f50a2adb362bd54f0a941ad16dac304510d579
Kevin Whitley <kevin3503@gmail.com>
2014-09-27 11:22:33 +0300

    cleaned up project

---
+++ test/data/classdata.js
@@ -0,0 +1,5 @@
+module.exports = [
+  ["Subject+:Courses:Date Added", "Subject+:Courses:Name", "Subject+:Courses:Description", "Subject+:Courses:Link", "Subject+:Courses:Media", "Providers:Name", "Name", "Subject+:Courses:Instructor", "Subject+:Courses:Start Date", "Subject+:Courses:Duration", "Subject+:Name"],
+  ["07/16/13", "9/11 and Its Aftermath -- Part I", "Provided by Duke University, this course investigates the forces leading up to the 9/11 attacks and the policies adopted by the US afterwards.", "https://www.coursera.org/course/911aftermath", "full course", "Coursera", "Duke University", "David Schanzer", "09/09/13", "7 weeks", "liberal-arts"],
+  ["07/16/13", "Analysis of a Complex Kind", "This course educates students on the subject of complex analysis, which is the study of functions that live in the complex plane.", "https://www.coursera.org/course/complexanalysis", "full course", "Coursera", "Wesleyan University", "Dr. Petra Bonfert-Taylor", "10/21/13", "6 weeks", "math"],
+];
--- test/test.js
+++ test/test.js
@@ -6,6 +6,7 @@ var welldata1 = require('./data/welldata1');
 var welldata2 = require('./data/welldata2');
 var arraywelldata = require('./data/arraywelldata');
 var arraywelldataNoHeaders = require('./data/arraywelldata-no-headers');
+var classdata = require('./data/classdata');
 
 describe('#getStats()', function() {
   var tree = new Treeize();
@@ -406,6 +407,39 @@ describe('#setSignature()', function() {
       ]).getData().should.eql([
         { foo: 'bar', log: [{ a: { b: 1 } }, { a: { b: 2 } }] },
         { foo: 'baz', log: [{ a: { b: 3 } }]}
+      ]);
+    });
+
+    it('+ modifier should work on deep edge (classdata) case', function() {
+      var tree = new Treeize();
+
+      tree.grow(classdata).getData().should.eql([
+        { Name: 'Duke University',
+          Subject:
+           [ { Name: 'liberal-arts',
+               Courses:
+                [ { 'Date Added': '07/16/13',
+                    Name: '9/11 and Its Aftermath -- Part I',
+                    Description: 'Provided by Duke University, this course investigates the forces leading up to the 9/11 attacks and the policies adopted by the US afterwards.',
+                    Link: 'https://www.coursera.org/course/911aftermath',
+                    Media: 'full course',
+                    Instructor: 'David Schanzer',
+                    'Start Date': '09/09/13',
+                    Duration: '7 weeks' } ] } ],
+          Providers: [ { Name: 'Coursera' } ] },
+        { Name: 'Wesleyan University',
+          Subject:
+           [ { Name: 'math',
+               Courses:
+                [ { 'Date Added': '07/16/13',
+                    Name: 'Analysis of a Complex Kind',
+                    Description: 'This course educates students on the subject of complex analysis, which is the study of functions that live in the complex plane.',
+                    Link: 'https://www.coursera.org/course/complexanalysis',
+                    Media: 'full course',
+                    Instructor: 'Dr. Petra Bonfert-Taylor',
+                    'Start Date': '10/21/13',
+                    Duration: '6 weeks' } ] } ],
+          Providers: [ { Name: 'Coursera' } ] }
       ]);
     });
 
